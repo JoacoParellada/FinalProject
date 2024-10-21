f82401d8725fd8b24dbb38576807034b01785cc1
Lauti14 <lautaro3314@hotmail.com>
2024-10-21 18:00:23 -0300

    creacion home

---
+++ final-project/src/components/screen/Home/Home.tsx
@@ -0,0 +1,11 @@
+import styles from './Home.module.css'
+
+export const Home = () => {
+	return (
+		<div className={styles.containerPrincipal}>
+			<div className={styles.containerLeft}>
+                <h1>Empresas</h1>
+            </div>
+		</div>
+	);
+};
