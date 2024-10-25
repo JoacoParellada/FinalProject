6a3ee72ac72e31c214d994aa227e533f22297f64
JoacoP <joaquiinparellada@gmail.com>
2024-10-25 16:25:11 -0300

    Cards Empresas

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import { Home } from "./components/screen/Home/Home";
 import { Sucursal } from "./components/screen/Sucursal/Sucursal";
 
 function App() {
-	return (
-		<div>
-			{/* <Home /> */}
-			<Sucursal/>
-		</div>
-	);
+  return (
+    <div>
+      <Home />
+      {/*<Sucursal/> */}
+    </div>
+  );
 }
 
 export default App;
--- src/components/modals/modalAgregarEmpresa/ModalAgregarEmpresa.tsx
+++ src/components/modals/modalAgregarEmpresa/ModalAgregarEmpresa.tsx
@@ -1,16 +1,53 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Button, Modal } from "react-bootstrap";
 import style from "./ModalAgregarEmpresa.module.css";
 
 interface ModalAgregarEmpresaProps {
   show: boolean;
   handleClose: () => void;
+  onSave: (empresa: {
+    nombre: string;
+    razonSocial: string;
+    cuit: string;
+    logo: string;
+  }) => void;
 }
 
 export const ModalAgregarEmpresa: FC<ModalAgregarEmpresaProps> = ({
   show,
   handleClose,
+  onSave,
 }) => {
+  const [empresa, setEmpresa] = useState({
+    nombre: "",
+    razonSocial: "",
+    cuit: "",
+    logo: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setEmpresa((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () =>
+        setEmpresa((prev) => ({ ...prev, logo: reader.result as string }));
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleSave = () => {
+    onSave(empresa);
+    handleClose();
+  };
+
   return (
     <Modal show={show} onHide={handleClose}>
       <Modal.Header className={style.modalHeader}>
@@ -23,7 +60,9 @@ export const ModalAgregarEmpresa: FC<ModalAgregarEmpresaProps> = ({
               type="text"
               className="form-control"
               placeholder="Ingrese un nombre"
+              name="nombre"
               required
+              onChange={handleChange}
             />
           </div>
           <div className="mb-3">
@@ -31,7 +70,9 @@ export const ModalAgregarEmpresa: FC<ModalAgregarEmpresaProps> = ({
               type="text"
               className="form-control"
               placeholder="Ingrese razon social"
+              name="razonSocial"
               required
+              onChange={handleChange}
             />
           </div>
           <div className="mb-3">
@@ -39,7 +80,9 @@ export const ModalAgregarEmpresa: FC<ModalAgregarEmpresaProps> = ({
               type="text"
               className="form-control"
               placeholder="Ingrese CUIT"
+              name="cuit"
               required
+              onChange={handleChange}
             />
           </div>
           <div className="mb-3">
@@ -47,6 +90,7 @@ export const ModalAgregarEmpresa: FC<ModalAgregarEmpresaProps> = ({
               type="file"
               className="form-control"
               placeholder="Ingrese una imagen"
+              onChange={handleImageChange}
             />
           </div>
         </form>
@@ -59,7 +103,11 @@ export const ModalAgregarEmpresa: FC<ModalAgregarEmpresaProps> = ({
         >
           CERRAR
         </Button>
-        <Button variant="success" className={style.confirmButton} type="submit">
+        <Button
+          variant="success"
+          className={style.confirmButton}
+          onClick={handleSave}
+        >
           CONFIRMAR
         </Button>
       </Modal.Footer>
