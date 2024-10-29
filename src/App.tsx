050f92473e703ffc3a3efcf3da0283635cad2f9d
Lauti14 <lautaro3314@hotmail.com>
2024-10-29 08:24:46 -0300

    terminado de rutas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { Home } from "./components/screen/Home/Home";
-import { Sucursal } from "./components/screen/Sucursal/Sucursal";
+
 import { AppRouter } from "./routes/AppRouter";
 
 function App() {
 	return (
-		//<div>
-		//	{ <Home /> }
-		//	<Sucursal/>
-		//  </div>
+		
 		<>
 			<AppRouter />
 		</>
