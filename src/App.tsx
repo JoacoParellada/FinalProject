08b6e7000b376f7dc9c8f4c7ca805c79faadc7a6
Lauti14 <lautaro3314@hotmail.com>
2024-10-29 08:10:30 -0300

    creacion de rutas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Home } from "./components/screen/Home/Home";
 import { Sucursal } from "./components/screen/Sucursal/Sucursal";
+import { AppRouter } from "./routes/AppRouter";
 
 function App() {
 	return (
-		<div>
-			{/* <Home /> */}
-			<Sucursal/>
-		</div>
+		//<div>
+		//	{ <Home /> }
+		//	<Sucursal/>
+		//  </div>
+		<>
+			<AppRouter />
+		</>
 	);
 }
 
