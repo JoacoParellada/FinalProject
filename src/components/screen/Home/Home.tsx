bdb9dafe4666dcd696db308cb3d9dfed558ac1f5
Marcos <marcosdimecom@gmail.com>
2024-11-07 17:01:43 -0300

    handle save arreglado

--- src/components/screen/Home/Home.tsx
+++ src/components/screen/Home/Home.tsx
@@ -5,45 +5,43 @@ import { useState, useEffect } from "react";
 import { CardEmpresa } from "../../CardEmpresa/CardEmpresa";
 import { EmpresaService } from "../../../services/EmpresaService";
 import { IEmpresa } from "../../../types/dtos/empresa/IEmpresa";
-import { useAppDispatch } from "../../../hooks/redux";
 
 export const Home = () => {
   const [showModal, setShowModal] = useState(false);
   const [empresas, setEmpresas] = useState<IEmpresa[]>([]);
   const empresaService = new EmpresaService("empresas")
-  const dispatch = useAppDispatch()
-  
 
+  const fetchEmpresas = async () => {
+    try {
+      const data = await empresaService.getAllEmpresas();
+      console.log("Empresas: ", data);
+      setEmpresas(data);
+      
+    } catch (error) {
+      console.log("Error fetching empresas:", error);
+    }
+  };
 
 
   useEffect(() => {
-    const fetchEmpresas = async () => {
-      try {
-        const data = await empresaService.getAllEmpresas();
-        console.log("Empresas: ", data);
-        setEmpresas(data);
-        
-      } catch (error) {
-        console.log("Error fetching empresas:", error);
-      }
-    };
+    
 
-fetchEmpresas();
-}, []);
+  fetchEmpresas();
+  }, []);
 
 const handleShow = () => setShowModal(true);
 const handleClose = () => setShowModal(false);
 
-// const handleSave = async (nuevaEmpresa: IEmpresa) => {
-// try {
-//   const createdEmpresa = await empresaService.post(nuevaEmpresa);
-//   if (createdEmpresa) {
-//         setEmpresas(prevEmpresas => [...prevEmpresas, createdEmpresa]);
-//       }
-// } catch (error) {
-//   console.error("Error saving new empresa:", error);
-// }
-// };
+  const handleSave = async (nuevaEmpresa: IEmpresa) => {
+    try {
+      await empresaService.post(nuevaEmpresa).then(()=>{
+        fetchEmpresas()
+      })
+    } catch (error) {
+      console.log("Error saving new empresa:", error);
+    }
+
+  };
 
   return (
     <div className={styles.containerPrincipal}>
@@ -63,11 +61,11 @@ const handleClose = () => setShowModal(false);
           <CardEmpresa key={index} empresa={empresa} />
         ))}
       </div>
-      {/* <ModalAgregarEmpresa
+      <ModalAgregarEmpresa
         show={showModal}
         handleClose={handleClose}
         onSave={handleSave}
-      /> */}
+      />
     </div>
   );
 };
