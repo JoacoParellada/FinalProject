eaf8c31c09e8a9de4397524e7f060095e7a462c5
Lauti14 <lautaro3314@hotmail.com>
2024-11-08 18:10:08 -0300

    actualizacion services

--- src/services/LocalidadesService.ts
+++ src/services/LocalidadesService.ts
@@ -3,14 +3,21 @@ import { BackendClient } from "./BackendClient";
 import { BASEURL } from "./BaseUrl";
 
 export class LocalidadesService extends BackendClient<ILocalidad> {
-    constructor() {
-        super(BASEURL  + "/localidades");
-    }
+	constructor() {
+		super(BASEURL + "/localidades");
+	}
 
-    
-    async getLocalidadesByProvincia(provinciaId: number): Promise<ILocalidad[]> {
-        const response = await fetch(`${this.baseUrl}/findByProvincia/${provinciaId}`);
-        const data = await response.json();
-        return data as ILocalidad[];
-    }
+	async getLocalidadesByProvincia(provinciaId: number): Promise<ILocalidad[]> {
+		const response = await fetch(
+			`${this.baseUrl}/findByProvincia/${provinciaId}`
+		);
+		const data = await response.json();
+		return data as ILocalidad[];
+	}
+
+	async getAll(): Promise<ILocalidad[]> {
+		const response = await fetch(`${this.baseUrl}`);
+		const data = await response.json();
+		return data as ILocalidad[];
+	}
 }
--- src/services/ProvinciaService.ts
+++ src/services/ProvinciaService.ts
@@ -3,14 +3,19 @@ import { BackendClient } from "./BackendClient";
 import { BASEURL } from "./BaseUrl";
 
 export class ProvinciaService extends BackendClient<IProvincia> {
-    constructor() {
-        super(BASEURL + "/provincias");
-    }
+	constructor() {
+		super(BASEURL + "/provincias");
+	}
 
-    
-    async getProvinciaByPais(paisId: number): Promise<IProvincia[]> {
-        const response = await fetch(`${this.baseUrl}/findByPais/${paisId}`);
-        const data = await response.json();
-        return data as IProvincia[];
-    }
+	async getProvinciaByPais(paisId: number): Promise<IProvincia[]> {
+		const response = await fetch(`${this.baseUrl}/findByPais/${paisId}`);
+		const data = await response.json();
+		return data as IProvincia[];
+	}
+
+	async getAllProvincias(): Promise<IProvincia[]> {
+		const response = await fetch(`${this.baseUrl}`);
+		const data = await response.json();
+		return data as IProvincia[];
+	}
 }
