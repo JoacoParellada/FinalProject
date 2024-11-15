8deeff23cf4f978824e13221a9f15a72d652c6d9
JoacoP <joaquiinparellada@gmail.com>
2024-11-15 17:26:44 -0300

    Funcion agregar categorias

--- src/services/CategoriaService.ts
+++ src/services/CategoriaService.ts
@@ -3,16 +3,42 @@ import { BackendClient } from "./BackendClient";
 import { BASEURL } from "./BaseUrl";
 
 export class CategoriaService extends BackendClient<ICategorias> {
+  constructor() {
+    super(BASEURL + "/categorias");
+  }
 
-    constructor(){
-        super(BASEURL + "/categorias")
+  async getAllCategoriasBySucursal(idSucursal: number): Promise<ICategorias[]> {
+    const response = await fetch(
+      `${this.baseUrl}/allCategoriasPorSucursal/${idSucursal}`
+    );
+    const data = await response.json();
+    return data as ICategorias[];
+  }
+
+  async getAllsubcategoriasByCategoriaPadre(
+    idSucursal: number,
+    idPadre: number
+  ): Promise<ICategorias[]> {
+    const response = await fetch(
+      `${this.baseUrl}/allSubCategoriasPorCategoriaPadre/${idPadre}/${idSucursal}`
+    );
+    const data = await response.json();
+    return data as ICategorias[];
+  }
+  async post(newCategoria: ICategorias): Promise<ICategorias> {
+    const response = await fetch(`${this.baseUrl}/create`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newCategoria),
+    });
+
+    if (!response.ok) {
+      throw new Error("Error al agregar la categoría");
     }
 
-    async getAllCategoriasBySucursal(idSucursal: number): Promise<ICategorias[]> {
-        const response = await fetch(`${this.baseUrl}/allCategoriasPorSucursal/${idSucursal}`);
-        const data = await response.json();
-        return data as ICategorias[];
-    }
-
-
+    const data = await response.json();
+    return data as ICategorias;
+  }
 }
